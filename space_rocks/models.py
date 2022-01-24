8e2e55a504de8e6f79a862971fcb912a13c9b0dd
Susan <susanj0918@gmail.com>
2022-01-24 12:47:56 -0500

    first addition

---
+++ space_rocks/models.py
@@ -0,0 +1,24 @@
+from pygame.math import Vector2
+
+from utils import load_sprite
+
+class GameObject:
+    def __init__(self, position, sprite, velocity):
+        self.position = Vector2(position)
+        self.sprite = sprite
+        self.radius = sprite.get_width() / 2
+        self.velocity = Vector2(velocity)
+
+    def draw(self, surface):
+        blit_position = self.position - Vector2(self.radius)
+        surface.blit(self.sprite, blit_position)
+
+    def move(self):n = self.position + self. velocity
+    
+    def collides_with(self, other_obj):
+        distance = self.position.distance_to(other_obj.position)
+        return distance < self.radius + other_obj.radius
+
+class Spaceship(GameObject):
+    def __init__(self, position):
+        super().__init__(position, load_sprite("spaceship"), Vector2(0))
