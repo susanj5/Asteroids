d3c93b6036bf96acdb2275d4c17ff86319f477f3
Susan Jiang <susanj0918@gmail.com>
2022-04-14 13:04:32 -0400

    adding requirements file, code, and assets

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -1,18 +1,42 @@
 import pygame
 
-from models import Spaceship
-from utils import load_sprite
+from models import Asteroid, Spaceship
+from utils import get_random_position, load_sprite, print_text, print_score, print_life
 
 class SpaceRocks:
+    min_asteroid_distance = 250
+    score = 100
+    life = 3
     def __init__(self):
         self._init_pygame()
         self.screen = pygame.display.set_mode((800,600))
         self.background = load_sprite("space", False)
         self.clock = pygame.time.Clock()
-        self.spaceship = Spaceship((400, 300))
-        #self.asteroid = GameObject(
-        #    (400, 300), pygame.transform.scale(load_sprite("asteroid"), (75,50)), (1, 0)
-        #)
+        self.font = pygame.font.Font(None, 64)
+        self.message = ""
+
+        self.asteroids = []
+        self.bullets = []
+        self.spaceship = Spaceship((400, 300), self.bullets.append)
+
+        for _ in range(6):
+            while True:
+                position = get_random_position(self.screen)
+                if (
+                    position.distance_to(self.spaceship.position)
+                    > self.min_asteroid_distance
+                ):
+                    break
+            
+            self.asteroids.append(Asteroid(position, self.asteroids.append))
+
+    def _get_game_objects(self):
+        game_objects = [*self.asteroids, *self.bullets]
+
+        if self.spaceship:
+            game_objects.append(self.spaceship)
+        
+        return game_objects
 
 
     def main_loop(self):
@@ -31,14 +55,69 @@ class SpaceRocks:
                 event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
             ):
                 quit()
+            elif (
+                self.spaceship
+                and event.type == pygame.KEYDOWN
+                and event.key == pygame.K_SPACE
+            ):
+                self.spaceship.shoot()
+                self.score -= 10
+        
+        is_key_pressed = pygame.key.get_pressed()
+
+        if self.spaceship:
+            if is_key_pressed[pygame.K_RIGHT]:
+                self.spaceship.rotate(clockwise=True)
+            elif is_key_pressed[pygame.K_LEFT]:
+                self.spaceship.rotate(clockwise=False)
+            if is_key_pressed[pygame.K_UP]:
+                self.spaceship.accelerate()
+
 
     def _process_game_logic(self):
-        self.spaceship.move()
-        self.asteroid.move()
+        for game_object in self._get_game_objects():
+            game_object.move(self.screen)
+        
+        if self.spaceship:
+            for asteroid in self.asteroids:
+                if asteroid.collides_with(self.spaceship):
+                    self.asteroids.remove(asteroid)
+                    asteroid.split()
+                    self.life -= 1
+                    if self.life == 0:
+                        self.spaceship = None
+                        self.message = "You lost!"
+                        break
+        
+        for bullet in self.bullets[:]:
+            for asteroid in self.asteroids[:]:
+                if asteroid.collides_with(bullet):
+                    self.asteroids.remove(asteroid)
+                    self.bullets.remove(bullet)
+                    asteroid.split()
+                    self.score += 50
+                    break
+
+        for bullet in self.bullets[:]:
+            if not self.screen.get_rect().collidepoint(bullet.position):
+                self.bullets.remove(bullet)
+        if not self.asteroids and self.spaceship:
+            self.message = "You won!"
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
-        self.spaceship.draw(self.screen)
-        self.asteroid.draw(self.screen)
+        
+        for game_object in self._get_game_objects():
+            game_object.draw(self.screen)
+        
+        if self.message:
+            if self.message == "You won!":
+                print_text(self.screen, self.message, self.font, True)
+            else:
+                print_text(self.screen, self.message, self.font)
+        
+        print_score(self.screen, self.score, self.font)
+        print_life(self.screen, self.life, self.font)
+
         pygame.display.flip()
         self.clock.tick(60)
--- space_rocks/models.py
+++ space_rocks/models.py
@@ -1,6 +1,9 @@
 from pygame.math import Vector2
+from pygame.transform import rotozoom
 
-from utils import load_sprite
+from utils import get_random_velocity, load_sound, load_sprite, wrap_position
+
+up = Vector2(0, -1)
 
 class GameObject:
     def __init__(self, position, sprite, velocity):
@@ -13,12 +16,75 @@ class GameObject:
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
 
-    def move(self):n = self.position + self. velocity
+    def move(self, surface):
+        self.position = wrap_position(self.position + self. velocity, surface)
     
     def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
         return distance < self.radius + other_obj.radius
 
 class Spaceship(GameObject):
-    def __init__(self, position):
+    maneuverability = 3
+    acceleration = 0.1
+    bullet_speed = 3
+
+    def __init__(self, position, create_bullet_callback):
+        self.create_bullet_callback = create_bullet_callback
+        self.laser_sound = load_sound("laser")
+
+        self.direction = Vector2(up)
+
         super().__init__(position, load_sprite("spaceship"), Vector2(0))
+
+    def rotate(self, clockwise=True):
+        sign = 1 if clockwise else -1
+        angle = self.maneuverability * sign
+        self.direction.rotate_ip(angle)
+    
+    def accelerate(self):
+        self.velocity += self.direction * self.acceleration
+
+    def shoot(self):
+        bullet_velocity = self.direction * self.bullet_speed + self.velocity
+        bullet = Bullet(self.position, bullet_velocity)
+        self.create_bullet_callback(bullet)
+        self.laser_sound.play()
+
+    def draw(self, surface):
+        angle = self.direction.angle_to(up)
+        rotated_surface = rotozoom(self.sprite, angle, 1.0)
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
+
+class Asteroid(GameObject):
+    def __init__(self, position, create_asteroid_callback, size=3):
+        self.create_asteroid_callback = create_asteroid_callback
+        self.size = size
+
+        size_to_scale = {
+            3: 1,
+            2: 0.5,
+            1: 0.25,
+        }
+        scale = size_to_scale[size]
+        sprite = rotozoom(load_sprite("asteroid"), 0, scale)
+
+        super().__init__(
+            position, sprite, get_random_velocity(1, 3)
+        )
+
+    def split(self):
+        if self.size > 1:
+            for _ in range(2):
+                asteroid = Asteroid(
+                    self.position, self.create_asteroid_callback, self.size - 1
+                )
+                self.create_asteroid_callback(asteroid)
+
+class Bullet(GameObject):
+    def __init__(self, position, velocity):
+        super().__init__(position, load_sprite("bullet"), velocity)
+    
+    def move(self, surface):
+        self.position = self.position + self.velocity
